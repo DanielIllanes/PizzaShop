aa54d1f3f07096e87861e48ceefa3c7a518ad3b1
Daniel Illanes <daniel.illanes@truextend.com>
2018-09-19 10:56:11 -0400

    Adding Unit Test for Ingredients

--- src/main/java/com/pizzashop/pizzashop/Repositories/IngredientsRepository.java
+++ src/main/java/com/pizzashop/pizzashop/Repositories/IngredientsRepository.java
@@ -3,8 +3,10 @@ package com.pizzashop.pizzashop.Repositories;
 import com.pizzashop.pizzashop.Models.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IngredientsRepository extends PagingAndSortingRepository<Ingredient,Long> {
+    Ingredient findByName(@Param("name") String name);
 }
